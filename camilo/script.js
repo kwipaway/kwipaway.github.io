d7e2228859e24707548672f7b909fd45efa98de8
kwipaway <111864085+kwipaway@users.noreply.github.com>
2023-11-16 04:18:43 -0600

    r

--- camilo/script.js
+++ camilo/script.js
@@ -3,7 +3,7 @@ const scene = new THREE.Scene();
 
 // Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
+camera.position.z = 100;
 
 // Create a renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
